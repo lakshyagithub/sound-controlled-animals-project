bc3e8ae3b968ffb42375c4aa6eb5f5e1191e1c0a
LAKSHYA SINGH CHAUHAN <85747258+lakshyagithub@users.noreply.github.com>
2022-02-27 10:36:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-
+var dog = 0;
+var cat = 0;
 function startClassification() {
     navigator.mediaDevices.getUserMedia({audio:true});
     classifier = ml5.soundClassifier('https://storage.googleapis.com/tm-model/EN7ttFmv2/model.json', modelReady);
@@ -12,7 +13,22 @@ function check(error,results) {
     }
     else{
         console.log(results);
+        r = Math.floor(Math.random()*255)+1;
+        g = Math.floor(Math.random()*255)+1;
+        b = Math.floor(Math.random()*255)+1;
         document.getElementById("result_label").innerHTML="I can hear : "+results[0].label;
         document.getElementById("result_confidence").innerHTML="Accuracy : "+(results[0].confidence*100).toFixed(2)+" %";
+        document.getElementById("result_label").style.color="rgb("+r+","+g+","+b+")";
+        document.getElementById("result_confidence").style.color="rgb("+r+","+g+","+b+")";
+        if(results[0].label == 'Meawing') {
+            document.getElementById("image_for_output").src="cat.gif";
+        }
+        else if(results[0].label == 'Braking') {
+            document.getElementById("image_for_output").src="Braking.jpg";
+        }
+        else if(results[0].label == 'Background Noise') {
+            document.getElementById("image_for_output").src="ear.png";
+        }
+        
     }
 }
